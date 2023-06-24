ec641b0e3763ede86bf4596cf54d468517879dd1
KoTeuKa404 <135438716+KoTeuKa404@users.noreply.github.com>
2023-06-24 16:42:09 +0300

    Update pin.py
    add code4

--- pin.py
+++ pin.py
@@ -1,6 +1,17 @@
 from pygame import*
 widht=600
 height=600
+class GameSprite(sprite.Sprite):
+    def __init_(self,player_image,player_x,player_y,player_speed,widht,heidht):
+        super().__init__()
+        self.image=transform.scale(image.load(player_image),(widht,heidht))
+        self.rect=self.image.get_rect()
+        self.x=player_x
+        self.y=player_y
+        self.speed=player_speed
+    def reset(self):
+        mw.blit(self.image,(self.rect.x,self.rect.y))
+
 
 mw=display.set_mode((widht,height))
 mw.fill((255,120,0))
