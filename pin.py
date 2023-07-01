25760d044c201005a98732761b9011d5eac2702e
KoTeuKa404 <135438716+KoTeuKa404@users.noreply.github.com>
2023-07-01 16:20:11 +0300

    Update pin.py
    add speed

--- pin.py
+++ pin.py
@@ -34,12 +34,29 @@ finish=False
 FPS=60
 racket1=Player("racket.png",50,200,4,50,150)
 racket2=Player("racket.png",350,200,4,50,150)
+speed_x=3
+speed_y=3
 
 while game:
     for e in event.get():
         if e.type==QUIT:
             game=False
 
+    
+    if finish!=True:
+        ball.rect.x+=speed_x
+        ball.rect.y+=speed_y
 
+
+
+    if ball.rect.y>widhth-50 or ball.rect.y<0:
+        speed_y*=-1
+
+
+
+
+
+    
+    ball update()    
     display.update()
     clock.tick(FPS) 
